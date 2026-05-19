9fc5aac35c4b2b1a7ed4ef1905efe6a3854d572a
felix2072 <info@felix-w.de>
2026-05-19 21:02:43 +0200

    repo purpose is to help developing addons and functions for blender

--- __init__.py
+++ __init__.py
@@ -1,24 +1,18 @@
-from . import addCube
-from . import deleteAll
-from . import unhide
+from . import icon_menu
 
 bl_info = {
     "name": "BlenderToolSet",
     "author": "Felix Worseck",
     "description": "Some usefull Functions to make Blender less anoying",
     "blender": (2, 80, 0),
-    "version": (2024, 11, 0),
+    "version": (2026, 5, 0),
     "category": "Outliner",
 }
 
 
 def register():
-    addCube.register()
-    deleteAll.register()
-    unhide.register()
+    icon_menu.register()
 
 
 def unregister():
-    addCube.unregister()
-    deleteAll.unregister()
-    unhide.unregister()
+    icon_menu.unregister()
